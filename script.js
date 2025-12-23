d4b2d83db8907988d64d77f627f39ab7500a978d
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-23 16:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let cost = 0;
+
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+
+    let first = arr.shift();
+    let second = arr.shift();
+
+    let sum = first + second;
+    cost += sum;
+    arr.push(sum);
+  }
+  return cost;
 }
 
 module.exports=mincost;
